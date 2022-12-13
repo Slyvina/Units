370766154aba7b85892cee26bae36d52e1fde66f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-13 22:55:37 +0100

    Test Volumes

---
+++ Test/TestVolumes/TestVolumes.cpp
@@ -0,0 +1,33 @@
+// Lic:
+// ***********************************************************
+// Units/Test/TestVolumes/TestVolumes.cpp
+// This particular file has been released in the public domain
+// and is therefore free of any restriction. You are allowed
+// to credit me as the original author, but this is not
+// required.
+// This file was setup/modified in:
+// 2022
+// If the law of your country does not support the concept
+// of a product being released in the public domain, while
+// the original author is still alive, or if his death was
+// not longer than 70 years ago, you can deem this file
+// "(c) Jeroen Broks - licensed under the CC0 License",
+// with basically comes down to the same lack of
+// restriction the public domain offers. (YAY!)
+// ***********************************************************
+// Version 22.12.13
+// EndLic
+#include <SlyvVolumes.hpp>
+using namespace std;
+using namespace Slyvina::Units;
+
+int main() {
+	auto V{ Volumes() };
+	for (auto v : *V) {
+		cout << v.first << " -> " << v.second << endl;
+	}
+
+	string p = "Slyvina:/Folder1/Folder2/Directory3/";
+	cout << p << "\t>>\t"<<AVolPath(p)<<endl;
+	return 0;
+}
