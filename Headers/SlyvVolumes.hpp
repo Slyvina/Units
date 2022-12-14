5fb269ef650e210b9943317f57596cac572783b1
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-14 09:30:50 +0100

    vUpdate

--- Headers/SlyvVolumes.hpp
+++ Headers/SlyvVolumes.hpp
@@ -1,7 +1,7 @@
 // Lic:
 // Units/Headers/SlyvVolumes.hpp
 // Slyvina - Volumes (header)
-// version: 22.12.13
+// version: 22.12.14
 // Copyright (C) 2022 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
