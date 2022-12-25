be19a853aad7f71bdbccc1cc661fde883f5bda5e
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-25 17:00:44 +0100

    Long live Visual Studio's auto indent 🤣

--- Headers/SlyvAsk.hpp
+++ Headers/SlyvAsk.hpp
@@ -24,25 +24,25 @@
 #include <SlyvConInput.hpp>
 
 namespace Slyvina {
-namespace Units{
-	extern GINIE AskGINIE;
-	extern std::string AskCat;
-	std::string CAsk(std::string Key, std::string Question, std::string DefaultValue = "");
-	std::string Ask(std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
-	std::string CAsk(GINIE Data, std::string Key, std::string Question, std::string DefaultValue = "");
-	std::string Ask(GINIE Data, std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
-	//std::string CAsk(std::shared_ptr<GINIE> Data,std::string Key, std::string Question, std::string DefaultValue = "");
-	//std::string Ask(std::shared_ptr<GINIE> Data, std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
+	namespace Units {
+		extern GINIE AskGINIE;
+		extern std::string AskCat;
+		std::string CAsk(std::string Key, std::string Question, std::string DefaultValue = "");
+		std::string Ask(std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
+		std::string CAsk(GINIE Data, std::string Key, std::string Question, std::string DefaultValue = "");
+		std::string Ask(GINIE Data, std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
+		//std::string CAsk(std::shared_ptr<GINIE> Data,std::string Key, std::string Question, std::string DefaultValue = "");
+		//std::string Ask(std::shared_ptr<GINIE> Data, std::string Cat, std::string Key, std::string Question, std::string DefaultValue = "");
 
 
-	bool Yes(GINIE Data, std::string Cat, std::string Key, std::string Question);
-	//bool Yes(std::shared_ptr<GINIE> Data, std::string Cat, std::string Key, std::string Question);
+		bool Yes(GINIE Data, std::string Cat, std::string Key, std::string Question);
+		//bool Yes(std::shared_ptr<GINIE> Data, std::string Cat, std::string Key, std::string Question);
 
 
-	int AskInt(GINIE Data, std::string Cat, std::string Key, std::string Question, bool AllowDefault=false, int DefaultValue=0);
-	int AskInt(GINIE Data, std::string Cat, std::string Key, std::string Question, int DefaultValue);
-	int AskInt(std::string Cat, std::string Key, std::string Question);
-	int AskInt(std::string Cat, std::string Key, std::string Question, int DefaultValue);
+		int AskInt(GINIE Data, std::string Cat, std::string Key, std::string Question, bool AllowDefault = false, int DefaultValue = 0);
+		int AskInt(GINIE Data, std::string Cat, std::string Key, std::string Question, int DefaultValue);
+		int AskInt(std::string Cat, std::string Key, std::string Question);
+		int AskInt(std::string Cat, std::string Key, std::string Question, int DefaultValue);
 
-}
+	}
 }
