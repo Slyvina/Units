36baeb278ee12041b517c56f5067587a56125416
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-25 18:44:53 +0100

    vUpdate

--- Source/SlyvAsk.cpp
+++ Source/SlyvAsk.cpp
@@ -1,7 +1,7 @@
 // Lic:
 // Units/Source/SlyvAsk.cpp
 // Ask
-// version: 22.12.24
+// version: 22.12.25
 // Copyright (C) 2022 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
