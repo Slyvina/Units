3eac8722564acda0b5e89d6b1e559ba3e2e50ca7
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-09-29 16:38:12 +0200

    vUpdate
    And #7 can now be merged.
    
    All that comes next is a later concern

--- Headers/SlyvRequestFile.hpp
+++ Headers/SlyvRequestFile.hpp
@@ -1,7 +1,7 @@
 // Lic:
 // Units/Headers/SlyvRequestFile.hpp
 // fsdf
-// version: 24.09.28
+// version: 24.09.29
 // Copyright (C) 2022, 2024 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
