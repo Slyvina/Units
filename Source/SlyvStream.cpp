529b926c080fbfe46e2791b4364081ca3a679822
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-06 22:31:13 +0200

    Windows Fuck

--- Source/SlyvStream.cpp
+++ Source/SlyvStream.cpp
@@ -271,7 +271,7 @@ namespace Slyvina {
 			char datestring[256];
 			stat(filename1, &st);
 #ifdef SlyvWindows
-			tm = _localtime(&st.st_mtime);
+			tm = _localtime(st.st_mtime);
 #else 
 			tm = localtime(&st.st_mtime);
 #endif
