8eb5078168d3f1b04c70f2ea6d182fbff4d01e68
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-06 22:24:46 +0200

    Header

--- Headers/SlyvTime.hpp
+++ Headers/SlyvTime.hpp
@@ -1,8 +1,8 @@
 // Lic:
 // Units/Headers/SlyvTime.hpp
 // Slyvina - Time (header)
-// version: 23.07.22
-// Copyright (C) 2021, 2022, 2023 Jeroen P. Broks
+// version: 24.10.06
+// Copyright (C) 2021, 2022, 2023, 2024 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
@@ -29,7 +29,9 @@ namespace Slyvina {
 		std::string CurrentTime();
 		std::string QTimeF(const char* f);
 		int CurrentYear();
+#ifdef SlyvWindows
 		tm _localtime(time_t* fuck);
+#endif
 		tm LocalTime();
 		time_t TimeStamp();
 	}
