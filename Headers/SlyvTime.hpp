c25965000079c8833159b1ad330fa5f7be591fdc
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-12-10 21:09:05 +0100

    Time update

--- Headers/SlyvTime.hpp
+++ Headers/SlyvTime.hpp
@@ -1,7 +1,7 @@
 // License:
 // 	Units/Headers/SlyvTime.hpp
 // 	Slyvina - Time (header)
-// 	version: 24.10.23
+// 	version: 24.12.07 II
 // 
 // 	Copyright (C) 2021, 2022, 2023, 2024 Jeroen P. Broks
 // 
@@ -21,25 +21,7 @@
 // 	   misrepresented as being the original software.
 // 	3. This notice may not be removed or altered from any source distribution.
 // End License
-// Lic:
-// Units/Headers/SlyvTime.hpp
-// Slyvina - Time (header)
-// version: 24.10.09
-// Copyright (C) 2021, 2022, 2023, 2024 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
+
 #pragma once
 
 #include <time.h>
@@ -52,9 +34,15 @@ namespace Slyvina {
 		std::string CurrentDate();
 		std::string CurrentTime();
 		std::string QTimeF(const char* f);
-		int CurrentYear();
-		int CurrentMonth();
-		int CurrentDay();
+		int CurrentYear(); // The year
+		int CurrentMonth(); // The number of the month
+		std::string CurrentMonthName();
+		int CurrentDay(); // Day in the month
+		int DayOfYear(); // Day of the year
+		int CurrentHour(); // Hour
+		int CurrentMinute(); // Minute
+		int CurrentSecond(); // Second
+		std::string WeekDay(); // Day of the week fully written out.
 #ifdef SlyvWindows
 		tm _localtime(time_t* fuck);
 #endif
--- Source/SlyvTime.cpp
+++ Source/SlyvTime.cpp
@@ -1,22 +1,26 @@
-// Lic:
-// Units/Source/SlyvTime.cpp
-// Slyvina - Time
-// version: 24.10.09
-// Copyright (C) 2021, 2022, 2023, 2024 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
+// License:
+// 	Units/Source/SlyvTime.cpp
+// 	Slyvina - Time
+// 	version: 24.12.07 II
+// 
+// 	Copyright (C) 2021, 2022, 2023, 2024 Jeroen P. Broks
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
 
 #include <string>
 #include <time.h>
@@ -123,6 +127,8 @@ namespace Slyvina {
 			}
 		}
 
+		std::string CurrentMonthName() { return QTimeF("%B"); }
+
 		int CurrentDay() {
 			char buff[256];
 #ifdef SlyvWindows
@@ -135,6 +141,43 @@ namespace Slyvina {
 			strftime(buff, 256, "%d", &loctime);
 			try {
 				return stoi(buff);
+			} catch (exception e) {
+				return 0;
+			}
+		}
+
+		int CurrentHour() {
+			try {
+				return stoi(QTimeF("%H"));
+			} catch (exception e) {
+				return 0;
+			}
+
+		}
+		int CurrentMinute() {
+			try {
+				return stoi(QTimeF("%M"));
+			} catch (exception e) {
+				return 0;
+			}
+
+		}
+		int CurrentSecond() {
+			try {
+				return stoi(QTimeF("%S"));
+			} catch (exception e) {
+				return 0;
+			}
+
+		}
+
+		std::string WeekDay() { return QTimeF("%A"); }
+
+	
+
+		int DayOfYear() {		
+			try {
+				return stoi(QTimeF("%j"));
 			} catch (exception e) {
 				return 0;
 			}
